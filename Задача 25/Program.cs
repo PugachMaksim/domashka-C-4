907be1602559f6d292a19edab17cae4b54085852
maksim pugach <you@example.com>
2023-03-20 23:05:25 +0300

    решил 27 задачу + добавил методы

--- Задача 25/Program.cs
+++ Задача 25/Program.cs
@@ -4,17 +4,41 @@
 2, 4 -> 16
 */
 
+// int A;
+// int B;
+// int sum = 1;
+// Console.WriteLine("Введите возводимое в степень число: ");
+// int.TryParse(Console.ReadLine(), out A);
+// Console.WriteLine("Введите степень для числа A: ");
+// int.TryParse(Console.ReadLine(), out B);
+// Console.Write($"{A} в степени {B} = ");
+//         for (int i = 1; i <= B; i++)
+//     {
+//        sum = sum * A;
+//     }
+//  Console.Write($"{sum}");
+
 int A;
 int B;
-int sum = 1;
+
 Console.WriteLine("Введите возводимое в степень число: ");
 int.TryParse(Console.ReadLine(), out A);
 Console.WriteLine("Введите степень для числа A: ");
 int.TryParse(Console.ReadLine(), out B);
-Console.Write($"{A} в степени {B} = ");
-        for (int i = 1; i <= B; i++)
-    {
-       sum = sum * A;
-    }
- Console.Write($"{sum}");
 
+int itg = stepen(A, B);
+
+int stepen(int chislo, int stepen)
+{
+   int result = 1;
+for (int i = 1; i <= stepen; i++)
+    {
+       result = result * chislo;
+    }
+     return itg = result;
+}
+
+Console.Write($"{A} в степени {B} = ");
+        
+ Console.Write($"{itg}");
+ 
--- Задача 27/Program.cs
+++ Задача 27/Program.cs
@@ -5,3 +5,30 @@
 9012 -> 12
 */
 
+// int a;
+// int n = 0;
+// Console.Write("Введите число: ");
+// int.TryParse(Console.ReadLine(), out a);
+// while (a > 0)
+// {
+//     n = n + a % 10;
+//     a = a / 10;
+// }
+// Console.WriteLine($"{a} -> {n}");
+
+int A;
+int n = 0;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine(), out A);
+Summa(A, n);
+
+int Summa(int chislo, int result)
+{
+    while (chislo > 0)
+ {
+     result = result + chislo % 10;
+     chislo = chislo / 10;
+ }
+ return n = result;
+}
+Console.WriteLine($"{A} -> {n}");
